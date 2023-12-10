25db7341c6232145a2f511109b649348582d5bb0
vincent-backend <yutaka@inc-acv.com>
2023-12-10 13:20:12 +0700

    index.html update

--- assets/js/maccmsv10.js
+++ assets/js/maccmsv10.js
@@ -1,32 +1,4 @@
-/* Piracy Detection */
-$("#btn-piracy-detection").click(function() {
-
-    $('#piracy_detection').css('display','block'); // to hide progress image
-    $('#piracy_detection_lose').css('display','none'); // to show lose image
-    $('.modal-subtitle').css({'color': '#05031A'});
-    $('.modal-subtitle').html("正在检测请勿关闭窗口…");
-    $('#progModal1').modal('show');
-    
-    // simulate starting
-    setTimeout(function() {
-        $('.modal-subtitle').html("开始加载特征文件…");
-    }, 2000);
-
-    // simulate finishing
-    setTimeout(function() {
-        $('.modal-subtitle').html("特征文件加载完毕，请稍后…");
-    }, 4000);
-
-    // simulate loss
-    setTimeout(function() {
-        $('#piracy_detection').css('display','none'); // to hide progress image
-        $('#piracy_detection_lose').css('display','block'); // to show lose image
-        $('.modal-subtitle').css({'color': '#F22F43'});
-        $('.modal-subtitle').html("未检测到苹果CMS");
-    }, 6000);
-});
-
-/* Documentation */
-$("#offcanvasDocumentMenu .nav-link").click(function(){
+/* Offcanvas */
+$("#offcanvasSidebarMenu .nav-link").click(function(){
     $('.offcanvas').offcanvas('hide');
 });
