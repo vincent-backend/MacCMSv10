b43202390cfbda4d26c5d7d5fa648bc3d370e3e2
vincent-backend <yutaka@inc-acv.com>
2023-12-11 19:12:39 +0700

    index.html complete

--- assets/js/maccmsv10.js
+++ assets/js/maccmsv10.js
@@ -2,3 +2,31 @@
 $("#offcanvasSidebarMenu .nav-link").click(function(){
     $('.offcanvas').offcanvas('hide');
 });
+
+$(document).ready(function(){
+    $('section').waypoint(function(direction) {
+        //var activeSection = $(this).next();
+        var activeSection = $(this);
+        if(direction === 'down'){
+            activeSection = $(this).next();
+        }
+        var sectionId   = this.element.id;
+        $('ul li').removeClass('active');
+        $('ul li.' + sectionId).addClass('active');
+    });
+});
+
+$('a[href*=\\#]:not([href=\\#])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+    || location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+       if (target.length) {
+         $('html,body').animate({
+             scrollTop: target.offset().top - (target.height() / 5)
+        }, 500);
+        return false;
+    }
+}
+});
